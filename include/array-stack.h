78800191376261b04cde142abac7be00d0070ee0
Abijith Anandakrishnan <46032120+Abijith97@users.noreply.github.com>
2026-02-19 19:46:14 +0000

    patch: fix header file

--- include/array-stack.h
+++ include/array-stack.h
@@ -2,5 +2,5 @@
 #include <stdlib.h>
 
 void push(int *arr_stack, int arr_size, int value, int *top);
-void pop(int *arr_stack, int arr_size, int *top);
+void pop(int *arr_stack, int *top);
 void display(int *arr_stack, int arr_size, int *top);
